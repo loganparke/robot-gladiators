4dd8f6a3c2b416a3758e0f00f95a7f34f5b1cfff
Logan Parke <logan.c.parke@gmail.com>
2022-01-30 22:58:31 -0800

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,18 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerHealth, playerAttack);
-
-var enemyName = "Roborto";
+var enemyNames = ["roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+for(var i =0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to fight or skip this battle? enter 'fight' or 'skip' to choose.");
@@ -59,5 +64,14 @@ var fight = function() {
       else {
         window.alert("You need to choose a valid option. Try again!");
       }
-    }
-fight();
+}
+
+for(var i=0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
